8b3d78ab04a22a82c4e2869cdd5454c0674477c7
ingridcriistine <ingridrochacristine@gmail.com>
2025-03-13 17:13:32 -0300

    finalizando backend

--- Front-end/src/components/card.tsx
+++ Front-end/src/components/card.tsx
@@ -9,11 +9,11 @@ interface Pokemon {
 
 export const Card = (pokemon: Pokemon) => {
     return (
-        <div className="bg-amber-200 w-[200px] p-4 h-[250px] m-4 rounded-2xl flex flex-col justify-center items-center">
-            <h2>{pokemon.name}</h2>
+        <div className="bg-amber-200 border-2 w-[200px] p-4 h-[250px] m-4 rounded-2xl flex flex-col justify-center items-center">
+            <h2 className="text-[18px] font-bold mb-4">{pokemon.name}</h2>
             <Image src={pokemon.image} alt="Imagem do pokemon"/>
-            <p>{pokemon.capture_rate}</p>
-            <p>{pokemon.id}</p>
+            <p className="mt-8 text-[15px]">Capture rate: {pokemon.capture_rate}</p>
+            <p className="text-[15px]">Id: {pokemon.id}</p>
         </div>
     )
 }
