350262a7c9632d5f1041b80eb6eac26a0bc6eefd
ingridcriistine <ingridrochacristine@gmail.com>
2025-03-20 15:34:19 -0300

    updates

--- Front-end/src/app/page.tsx
+++ Front-end/src/app/page.tsx
@@ -1,10 +1,57 @@
-import { Card } from "@/components/card";
+'use client'
+
+import { Card, Pokemon } from "@/components/card";
+import { useState, useEffect  } from "react";
+import {api} from "./constants/api"
 import Image from "next/image";
+import title from "../assets/title.png";
 
 export default function Home() {
+  const [cardsData, setCardsData] =  useState<Pokemon[]>([]);
+  const [error, setError] = useState();
+  const [page, setPage] = useState<string>("")
+
+  useEffect(() =>{
+    api.get(`pokemon?limit=10&offset=${page}`).then((res) => {
+       res.data.results.forEach((pokemon: { url: string; name : string;}) => {
+         api.get(pokemon.url.replace('https://pokeapi.co/api/v2/', ''))
+         .then((info) => {
+           console.log(info.data)
+           let newlist = cardsData;
+            api.get(`pokemon-species/${info.data.id}`)
+            .then((rate) => {
+              console.log(rate.data.capture_rate);
+              newlist.push({name: info.data.name, id: info.data.id, image: "", capture_rate: rate.data.capture_rate});
+              setCardsData(newlist);
+            })
+         })
+       });
+     })
+     
+     .then (()  => {
+       cardsData.sort();
+      })
+
+   }, [page])
+
   return (
-    <div>
-        <Card id={2} name={"Pokemon"} image={""} capture_rate={15}/>
+    <div className="h-full pl-42 pr-42" style={{backgroundImage: `url("https://wallup.net/wp-content/uploads/2017/11/17/301412-Pokemon.jpg")`}}>
+      <div className="flex justify-center">
+        <Image src={title} alt="" className="w-[200px]"/>
+      </div>
+      <div className="flex flex-wrap justify-center w-full pb-12">
+        {cardsData.map((item, index) => {
+            return( 
+              <Card 
+                key={index}
+                id={item.id}
+                name={item.name} 
+                image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png`}
+                capture_rate={item.capture_rate}
+              />
+            );
+        })}
+      </div>
     </div>
   );
 }
--- Front-end/src/components/card.tsx
+++ Front-end/src/components/card.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-interface Pokemon {
+export interface Pokemon {
     id: number,
     name: string,
     image: string,
@@ -9,11 +7,15 @@ interface Pokemon {
 
 export const Card = (pokemon: Pokemon) => {
     return (
-        <div className="bg-amber-200 border-2 w-[200px] p-4 h-[250px] m-4 rounded-2xl flex flex-col justify-center items-center">
-            <h2 className="text-[18px] font-bold mb-4">{pokemon.name}</h2>
-            <Image src={pokemon.image} alt="Imagem do pokemon"/>
-            <p className="mt-8 text-[15px]">Capture rate: {pokemon.capture_rate}</p>
+        <div
+            className="border-2 border-blue-800 w-[250px] p-6 m-2 rounded-2xl flex flex-col justify-center items-center"
+            style={{ backgroundColor: 'rgba(255, 213, 79, 0.75)' }} // Cor bg-amber-200 com 25% de opacidade
+            >
+            <h2 className="text-[18px] font-bold capitalize">{pokemon.name}</h2>
+            <img src={pokemon.image} className="w-54" alt="Imagem do pokemon" width={1000} height={1000}/>
+            <p className="text-[15px]">Capture rate: {pokemon.capture_rate}</p>
             <p className="text-[15px]">Id: {pokemon.id}</p>
+            <button className="m-4 font-bold bg-blue-800 text-white p-1 rounded-[5px] w-32 opacity-80 hover:opacity-100 cursor-pointer">Capture</button>
         </div>
     )
 }
